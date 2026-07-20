84ea3b1bb1773ccf865aad990ae0c414e94a4452
TiRADfolk <Tiradfolk@gmail.com>
2026-07-20 07:19:57 +0200

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,65 @@
+export interface SiteDesign {
+  heroBackgroundImage: string;
+  overlayOpacity: string;
+}
+
+export interface ReseauSocial {
+  nom: string;
+  url: string;
+  icone: string;
+}
+
+export interface SiteInfos {
+  nom: string;
+  slogan: string;
+  logo: string;
+  descriptionLongue: string;
+  emailContact: string;
+  telephone: string;
+  reseauxSociaux: ReseauSocial[];
+  lienMedia: string;
+  presentationTitre: string;
+  presentationTexte: string;
+  design: SiteDesign;
+}
+
+export interface NewsItem {
+  afficherSurAccueil: boolean;
+  titre: string;
+  description: string;
+  lien?: string;
+  image?: string;
+}
+
+export interface Activite {
+  id: string;
+  titre: string;
+  description: string;
+  image?: string;
+}
+
+export interface BoutonAgenda {
+  label: string;
+  url: string;
+}
+
+export interface EvenementAgenda {
+  id: string;
+  date: string;
+  title: string;
+  location: string;
+  lieuPrecise?: string;
+  description: string;
+  estPublic: boolean;
+  tarif: boolean | string;
+  logoEvenement?: string; // Peut être un Emoji OU une URL d'image
+  boutons: BoutonAgenda[];
+}
+
+export interface MembreTrombi {
+  id: string;
+  nom: string;
+  role: string;
+  description: string;
+  photoUrl: string;
+}
