4f943e1128f7a86de4804c46b2cb52331a35983f
TiRADfolk <Tiradfolk@gmail.com>
2026-07-22 14:00:23 +0200

    Update page.tsx

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -1,12 +1,9 @@
 // src/app/media/page.tsx
 
-import Link from 'next/link';
 import { getMedias } from '../../data';
 import MediaClientContent from './MediaClientContent';
 
 export default async function MediaPage() {
-  // Récupération des données côté Serveur
-  const medias = await getMedias();
-
+  const medias = (await getMedias()) || [];
   return <MediaClientContent initialMedias={medias} />;
 }
